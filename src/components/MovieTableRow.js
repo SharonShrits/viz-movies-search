85d5a4a9f8628ed9ac6406de7e872455e94e66d1
Sharon Shrits <sharonshrits15@gmail.com>
2021-09-01 02:33:18 +0300

    Fixes

--- src/components/MovieTableRow.js
+++ src/components/MovieTableRow.js
@@ -2,13 +2,11 @@ import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-const MovieTableRow = ({ movie }) => {
-    return (
-        <TableRow key={movie.imdbID}>
-            <TableCell component="th" scope="row">{movie.Title}</TableCell>
-            <TableCell align="right">{movie.Year}</TableCell>
-        </TableRow>
-    );
-}
+const MovieTableRow = ({ movie: { imdbID, Title, Year } }) => (
+    <TableRow key={imdbID}>
+        <TableCell component="th" scope="row">{Title}</TableCell>
+        <TableCell align="right">{Year}</TableCell>
+    </TableRow>
+);
 
 export default MovieTableRow;
